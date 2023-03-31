e76fa55d4c6ee16b8bd8d603638fceb8f3f77041
Ameya Bapaye <ameyabapaye@gmail.com>
2023-03-31 15:44:57 -0700

    Added initial capsule code

---
+++ capsule/rebeccameya.seasonal_produce/code/FindProduceForSeason.js
@@ -0,0 +1,11 @@
+export default function (input) {
+  
+  // Call to database to get produce for input season
+  // Return produce
+  
+  return {
+      produceName : "Apple",
+      produceType: "Fruit",
+      season: ["Spring", "Summer", "Fall", "Winter"]
+  }
+}
